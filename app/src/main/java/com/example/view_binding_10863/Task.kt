0a970f3ab3551af5de235262ae44a29ab20256e8
tangkasR <tangkasrisdianto2@gmail.com>
2022-09-14 13:53:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10863/Task.kt
@@ -0,0 +1,3 @@
+package com.example.view_binding_10863
+
+    data class Task (val chapter:String, val title:String, val details: String)
---
+++ app/src/main/java/com/example/view_binding_10863/TaskList.kt
@@ -0,0 +1,26 @@
+package com.example.view_binding_10863
+
+object TaskList{
+    val taskList = listOf<Task>(
+        Task(
+            "Chapter 1", "Pagi",
+            "Bangun buat siap-siap sesi satu kuliah"
+        ),
+        Task(
+            "Chapter 2", "Siang",
+            "Makan siang bareng temen-temen"
+        ),
+        Task(
+            "Chapter 3", "Sore",
+            "melatih otot di gym"
+        ),
+        Task(
+            "Chapter 4", "Malam",
+            "Mengerjakan tugas-tugas kuliah sampai mata panda"
+        ),
+        Task(
+            "Chapter 5", "Besok",
+            "Mengulangi kegiatan setiap hari"
+        )
+    )
+}
